5ca137f7533478d2531f288984c77d285095a8ce
daniel castillo <a2020114119@estudiantes.upsa.edu.bo>
2021-10-13 19:59:41 -0400

    migratios implement

---
+++ Practica/Migrations/20211013235826_AddModels.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Practica.Migrations
+{
+    public partial class AddModels : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Practica",
+                columns: table => new
+                {
+                    NameId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nombre = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Enlace = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Practica", x => x.NameId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Practica");
+        }
+    }
+}
