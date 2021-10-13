ee8c9950eb7aea883e12945c0f7d8649ce84938e
daniel castillo <a2020114119@estudiantes.upsa.edu.bo>
2021-10-13 19:38:54 -0400

    primer comit settings listo y models listo

---
+++ Practica/Models/Practica.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Practica.Models
+{
+    public class Practica
+    {
+        [Key]
+        private static readonly string[] Summaries = new[]
+        {
+            "A", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"
+        };
+
+        [Required(ErrorMessage ="Requerido")]
+        [Display(Name ="trebol, espada, diamante, unicornio")]
+        public string Nombre { get; set; }
+
+        [Url]
+        [StringLength(maximumLength:200,MinimumLength =2)]
+
+        public string Enlace { get; set; }
+
+    }
+}
